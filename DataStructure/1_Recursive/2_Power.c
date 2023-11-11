ff913ba4a4bc9b3646cece747391f061fb0a0b4a
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2023-11-11 15:13:31 +0900

    Update 2_Power.c

--- DataStructure/1_Recursive/2_Power.c
+++ DataStructure/1_Recursive/2_Power.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int pow(int x , int n){
+    if(n == 0){
+        return 1;
+    }
+    else if(n % 2 == 0){
+        return pow(x * x , n / 2);
+    }
+    else{
+        return x * pow(x * x / (n - 1) / 2);
+    }
+}
+
+int pow_iter(int x , int n){
+    int res = 1;
+    for(int i=0; i<n; i++){
+        res *= x;
+    }
+    return res;
+}
+
+int main(){
+    return 0;
+}
